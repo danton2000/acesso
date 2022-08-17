2adb74ca4f980c0236f3e1cd813b52b070978f12
Danton Rodrigues <dantoncampos@hotmail.com>
2022-08-17 19:24:48 -0300

    subindo projeto

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
+    
+# @app.route("/about/")
+# def about():
+#     return render_template("about.html")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+
+    usuarios = ['gabriela', 'enzo', 'donizete', 'larissa', 'lucas', 'danton']
+
+    if nome_usuario in usuarios:
+
+        return render_template("acesso.html", nome_usuario = nome_usuario)
+            
+    else:
+        return """
+            <h1>Acesso negado!</h1>
+        """
